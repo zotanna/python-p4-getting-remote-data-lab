e853dc732baa60af089a02665cbf1da75fe8fdb6
denis <denniiskim@gmail.com>
2024-03-04 04:01:40 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        employees = []
+
+        employees_list = json.loads(self.get_response_body())
+        for employee in employees_list:
+            employees.append(employee)
+        return employees
